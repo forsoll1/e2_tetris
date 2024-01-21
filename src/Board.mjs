78085492b62837c580fbb41c9543ee78029b3bb7
Olli Forssell <olli20112@student.hamk.fi>
2024-01-21 19:37:20 +0200

    started removing code related to old board

--- src/Board.mjs
+++ src/Board.mjs
@@ -23,13 +23,6 @@ export class Board {
   
   drop(shape){
     let middle = Math.floor(this.width/2)
-    let firstRow = ""
-    for (let index = 0; index < this.width; index++) {
-      if (index != middle){firstRow += "."}
-      else{firstRow += shape}
-    }
-    firstRow += "\n"
-    this.board[0] = firstRow
     // for new board
     this.newBoard[0][middle] = shape
   }
@@ -42,7 +35,6 @@ export class Board {
     }
 
     return boardLines.join('')
-    //return this.board.join('')
   }
 
 }
