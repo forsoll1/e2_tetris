81d9c328f4d99d4c717fd023535a1f5effb48654
Olli Forssell <olli20112@student.hamk.fi>
2024-01-21 18:54:54 +0200

    first drop function

--- src/Board.mjs
+++ src/Board.mjs
@@ -19,6 +19,14 @@ export class Board {
 
   
   drop(shape){
+    let middle = Math.floor(this.width/2)
+    let firstRow = ""
+    for (let index = 0; index < this.width; index++) {
+      if (index != middle){firstRow += "."}
+      else{firstRow += shape}
+    }
+    firstRow += "\n"
+    this.board[0] = firstRow
   }
 
   toString() {
