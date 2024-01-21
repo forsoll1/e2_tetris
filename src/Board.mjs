4c4b309a4d0733f6f2295c19c00e48d4bde0f0fb
Olli Forssell <olli20112@student.hamk.fi>
2024-01-21 19:38:10 +0200

    further removed old board code

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,21 +1,17 @@
 export class Board {
   width;
   height;
-  board;
   newBoard;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.board = new Array();
     this.newBoard = new Array();
     this.initBoard()
   }
 
   initBoard(){
-    let row = ".".repeat(this.width) + "\n"
     for (let index = 0; index < this.height; index++) {
-      this.board.push(row)
       this.newBoard.push(Array(this.width).fill("."))
     }
   }
@@ -23,7 +19,6 @@ export class Board {
   
   drop(shape){
     let middle = Math.floor(this.width/2)
-    // for new board
     this.newBoard[0][middle] = shape
   }
 
@@ -33,7 +28,6 @@ export class Board {
       let newLine = line.join('') + "\n"
       boardLines.push(newLine)
     }
-
     return boardLines.join('')
   }
 
