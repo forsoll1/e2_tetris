af67c557e022d1bd639db1ce47152b0c7690f3a2
Olli Forssell <olli20112@student.hamk.fi>
2024-01-21 18:09:34 +0200

    moved board initialization to a new method

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,18 +1,24 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.initBoard()
+  }
+
+  initBoard(){
+    let row = ".".repeat(this.width) + "\n"
+    this.board = ""
+    for (let index = 0; index < this.height; index++) {
+      this.board += row
+    }
   }
 
   toString() {
-    let row = ".".repeat(this.width) + "\n"
-    let board = ""
-    for (let index = 0; index < this.height; index++) {
-      board += row
-    }
-    return board
+    return this.board
   }
+
 }
