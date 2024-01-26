51b7ccb89e14dfbbdd6dfb8f23871b91d4048b60
Olli Forssell <olli20112@student.hamk.fi>
2024-01-26 19:04:33 +0200

    Started migrating code from RotatingShape to Tetromino

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -3,4 +3,19 @@ import {RotatingShape} from "./RotatingShape.mjs"
 export class Tetromino{
    static T_SHAPE = new RotatingShape(".T.\nTTT\n...")
    static I_SHAPE = new RotatingShape(".....\n.....\nIIII.\n.....\n.....")
+
+   objectArray;
+
+   constructor(shape){
+       this.objectArray = []
+       let shapeAsArray = shape.replace(/\t| |\n$/g, '').split("\n")
+       for (let line = 0; line < shapeAsArray.length; line++) {
+           let row = []
+           for (let char = 0; char < shapeAsArray[line].length; char++) {
+               row.push(shapeAsArray[line][char])
+           }
+           this.objectArray.push(row)
+       }
+   }
+
 }
