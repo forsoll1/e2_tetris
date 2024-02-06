c297eb2edf1d2044a8c4c7596eb764c21d9eeea3
Olli Forssell <olli20112@student.hamk.fi>
2024-02-06 16:49:38 +0200

    According to ARS the ceiling blocks rotation so adding a test for that

--- test/RotatingFallingTetrominoes.test.mjs
+++ test/RotatingFallingTetrominoes.test.mjs
@@ -2,6 +2,7 @@ import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
+import { NewTetromino } from "../src/NewTetromino.mjs";
 
 function fallToBottom(board) {
   for (let i = 0; i < 10; i++) {
@@ -15,13 +16,27 @@ describe("Falling rotating tetrominoes", () => {
     board = new Board(10, 6);
   });
 
+  test("Tetromino can't rotate when against the ceiling", () => {
+    board.drop(NewTetromino.T_SHAPE);
+    board.rotateLeft()
+
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ....TT....
+       ....T.....
+       ..........
+       ..........
+       ..........`
+    );
+  });
+
   test("Tetromino can rotate left", () => {
     board.drop(Tetromino.T_SHAPE);
     board.rotateLeft()
 
     expect(board.toString()).to.equalShape(
       `....T.....
-       ...TT.....
+       ....TT....
        ....T.....
        ..........
        ..........
