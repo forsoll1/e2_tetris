b14ed020679e76c3847a4ed3bd95701ea85f9c8f
Olli Forssell <olli20112@student.hamk.fi>
2024-02-06 16:42:50 +0200

    O shape tests refactored

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -83,10 +83,8 @@ describe("The I shape", () => {
   });
 });
 
-/*
-
 describe("The O shape", () => {
-  const shape = Tetromino.O_SHAPE;
+  const shape = NewTetromino.O_SHAPE;
 
   test("initial orientation", () => {
     expect(shape.toString()).to.equalShape(
@@ -116,4 +114,3 @@ describe("The O shape", () => {
     expect(distinctOrientations(shape).size).to.equal(1);
   });
 });
-*/
