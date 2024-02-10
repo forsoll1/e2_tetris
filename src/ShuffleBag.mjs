94a75d6fb9abea098ef7ea400b41a558e35d23b4
Olli Forssell <olli20112@student.hamk.fi>
2024-02-10 19:20:49 +0200

    established shufflebag class

--- src/ShuffleBag.mjs
+++ src/ShuffleBag.mjs
@@ -1,3 +1,13 @@
 export class ShuffleBag{
-    
+
+    itemList;
+    currentPos;
+    currentItem;
+    size;
+
+    constructor(itemArray){
+        this.itemList = itemArray
+        this.currentPos = itemArray.length -1
+        this.size = itemArray.length
+    }
 }
