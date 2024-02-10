7289aa803baedb53260c4a60fe0a9e38591f9817
Olli Forssell <olli20112@student.hamk.fi>
2024-02-10 19:38:31 +0200

    testing revealed that I had failed to make a deep copy of given list of objects in shuffle bag constructor. Testing prevails again. Also 'shufflebag returns all inserted items' passes

--- src/ShuffleBag.mjs
+++ src/ShuffleBag.mjs
@@ -6,18 +6,24 @@ export class ShuffleBag{
     size;
 
     constructor(itemArray){
-        this.itemList = itemArray
+        this.itemList = []
+        for (let i = 0; i < itemArray.length; i++) {
+            this.itemList.push(itemArray[i])
+        }
         this.currentPos = itemArray.length -1
         this.size = itemArray.length
     }
+
     next(){
         if(this.currentPos < 1){
             this.currentPos = this.size -1
             this.currentItem = this.itemList[0]
-            return this.currentItem}
+            return this.currentItem
+        }
         let pos = Math.floor(Math.random() * this.currentPos)
         this.currentItem = this.itemList[pos]
         this.itemList[pos] = this.itemList[this.currentPos]
         this.currentPos -= 1
-        return this.currentItem}
+        return this.currentItem
+    }
 }
--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -1,17 +1,30 @@
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { ShuffleBag } from "../src/ShuffleBag.mjs"
+import { before } from "lodash";
 
 describe("Testing randomness", () => {
     let arr1 = [1,2,3,4,5,6,7,8,9,10]
+    let arr2;
+    let bag
+    beforeEach(() => {
+        bag = new ShuffleBag(arr1)
+    })
 
     test("Shufflebag returns random order", () => {
-        let bag = new ShuffleBag(arr1)
-        let arr2 = []
+        arr2 = []
         for (let i = 0; i < arr1.length; i++) {
             arr2.push(bag.next())
         }
         expect(arr1).to.not.include(arr2)
+    })
+
+    test("Shufflebag returns all inserted items", () => {
+        arr2 = []
+        for (let i = 0; i < arr1.length; i++) {
+            arr2.push(bag.next())
+        }
+        expect(arr2).to.have.members(arr1)
     })
     
 })
